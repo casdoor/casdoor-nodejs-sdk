4307553c5ae0b703ff772a354e02fb16d015ba25
Taoning Ge <gtn1024o@outlook.com>
2022-10-02 14:24:31 +0800

    feat: nullable to 3-rd party properties (#22)
    * fix: nullable to 3-rd party properties
    
    * feat: sync fields from go

--- src/sdk.ts
+++ src/sdk.ts
@@ -21,45 +21,77 @@ export interface User {
   name: string
   createdTime: string
   updatedTime: string
+
   id: string
   type: string
-  password: string
-  displayName: string
-  avatar: string
-  permanentAvatar: string
+  password?: string
+  passwordSalt?: string
+  displayName?: string
+  firstName?: string
+  lastName?: string
+  avatar?: string
+  permanentAvatar?: string
   email: string
-  phone: string
-  location: string
+  emailVerified: boolean
+  phone?: string
+  location?: string
   address: string[]
-  affiliation: string
-  title: string
-  homepage: string
-  bio: string
-  tag: string
-  region: string
+  affiliation?: string
+  title?: string
+  idCardType?: string
+  idCard?: string
+  homepage?: string
+  bio?: string
+  tag?: string
+  region?: string
   language: string
+  gender?: string
+  birthday?: string
+  education?: string
   score: number
+  karma: number
   ranking: number
+  isDefaultAvatar: boolean
   isOnline: boolean
   isAdmin: boolean
   isGlobalAdmin: boolean
   isForbidden: boolean
+  isDeleted: boolean
   signupApplication: string
-  hash: string
-  preHash: string
-  github: string
-  google: string
-  qq: string
-  wechat: string
-  facebook: string
-  dingtalk: string
-  weibo: string
-  gitee: string
-  linkedin: string
-  wecom: string
-  lark: string
-  gitlab: string
-  ldap: string
+  hash?: string
+  preHash?: string
+
+  createdIp?: string
+  lastSigninTime?: string
+  lastSigninIp?: string
+
+  github?: string
+  google?: string
+  qq?: string
+  wechat?: string
+  facebook?: string
+  dingtalk?: string
+  weibo?: string
+  gitee?: string
+  linkedin?: string
+  wecom?: string
+  lark?: string
+  gitlab?: string
+  adfs?: string
+  baidu?: string
+  alipay?: string
+  casdoor?: string
+  infoflow?: string
+  apple?: string
+  azuread?: string
+  slack?: string
+  steam?: string
+  bilibili?: string
+  okta?: string
+  douyin?: string
+  custom?: string
+
+  ldap?: string
   properties: Record<string, string>
 }
 
